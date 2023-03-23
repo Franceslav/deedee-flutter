4dd2f2ce6c3e79c9516e0a48916bdea9a82b8d6a
kyrgyz00 <bekbolottashpolotov@gmail.com>
2023-03-23 12:36:06 +0600

    DEEMOB-159-deleted-extra-import

--- packages/search_address_repository/lib/src/search_address_repository.dart
+++ packages/search_address_repository/lib/src/search_address_repository.dart
@@ -2,8 +2,6 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:latlong2/latlong.dart';
-import 'package:http/http.dart' as http;
-import 'package:latlong2/latlong.dart';
 
 import 'package:search_address_repository/search_address_repository.dart';
 
