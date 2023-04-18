5f013d4091c44882240d9e62c4d7f15b1140fe9d
Bohdan Kim <user576g@gmail.com>
2023-04-18 16:37:26 +0300

    fix: tags were not properly deleted because userId was not passed.

--- lib/repository/tag_repository.dart
+++ lib/repository/tag_repository.dart
@@ -1,7 +1,9 @@
 import 'package:deedee/generated/AccountService.pbenum.dart';
+import 'package:deedee/generated/filter_service.pb.dart';
 import 'package:deedee/generated/tag_service.pb.dart';
 import 'package:deedee/generated/tag_service.pbgrpc.dart';
 import 'package:deedee/generated/timestamp.pb.dart';
+import 'package:deedee/generated/topic_service.pb.dart';
 import 'package:deedee/injection.dart';
 import 'package:deedee/model/user.dart';
 import 'package:deedee/services/client_channel.dart';
@@ -46,14 +48,18 @@ class TagRepository {
   // }
 
   Future<List<Tag>> getTags(String userId) async {
-    final response = await _tagServiceClient
-        .getTags(TagRequest()..tag = Tag(status: Tag_Status.PLACED));
+    final response = await _tagServiceClient.getTags(
+        TagRequest(tag: Tag(compositeFilter: CompositeFilter(topic: Topic(userId: userId))))
+    );
     return response.tags;
   }
 
   Future<Tag> deleteTag(String userId, Int64 tagId) async {
-    final response = await _tagServiceClient
-        .removeTag(TagRequest()..tag = Tag(tagId: tagId));
+    final response = await _tagServiceClient.removeTag(
+        TagRequest(tag: Tag(
+            tagId: tagId, compositeFilter: CompositeFilter(topic: Topic(userId: userId))
+        ))
+    );
     return response.tags.first;
   }
 
--- lib/services/fake/mock_tag_service_client.dart
+++ lib/services/fake/mock_tag_service_client.dart
@@ -134,75 +134,13 @@ class MockTagServiceClient implements TagServiceClient {
         FakeClientCall<dynamic, TagResponse>(_getTags(request)));
   }
 
+  String retrieveUserIdFrom(TagRequest request) =>
+      request.tag.compositeFilter.topic.userId;
+
   Future<TagResponse> _getTags(TagRequest request) async {
-    // var geoLocation1 = Geolocation()
-    //   ..latitude = 51.51
-    //   ..longitude = -0.1165888;
-
-    // var geoLocation2 = Geolocation()
-    //   ..latitude = 51.521
-    //   ..longitude = -0.116;
-
-    // var geoLocation3 = Geolocation()
-    //   ..latitude = 51.53
-    //   ..longitude = 0.1167;
-
-    // var geoLocation4 = Geolocation()
-    //   ..latitude = 51.52134
-    //   ..longitude = -0.1169;
-
-    // var geoLocation5 = Geolocation()
-    //   ..latitude = 51.522
-    //   ..longitude = 0.117;
-
-    // var timestamp1 = Timestamp()
-    //   ..seconds = Int64.parseInt(
-    //       (DateTime.now().millisecondsSinceEpoch / 1000).round().toString());
-    // var timestamp2 = Timestamp()
-    //   ..seconds = Int64.parseInt(
-    //       (DateTime.now().millisecondsSinceEpoch / 1000).round().toString());
-    // var timestamp3 = Timestamp()
-    //   ..seconds = Int64.parseInt(
-    //       (DateTime.now().millisecondsSinceEpoch / 1000).round().toString());
-    // var timestamp4 = Timestamp()
-    //   ..seconds = Int64.parseInt(
-    //       (DateTime.now().millisecondsSinceEpoch / 1000).round().toString());
-
-    // var tags = [
-    //   Tag()
-    //     ..tagId = Int64()
-    //     ..createdBy = Int64()
-    //     ..createdAt = timestamp1
-    //     ..compositeFilter = CompositeFilter()
-    //     ..geolocation = geoLocation1
-    //     ..type = Tag_Type.SUPPLIER
-    //     ..status = Tag_Status.BOOKMARKED,
-    //   Tag()
-    //     ..tagId = Int64()
-    //     ..createdBy = Int64()
-    //     ..createdAt = timestamp2
-    //     ..compositeFilter = CompositeFilter()
-    //     ..geolocation = geoLocation2
-    //     ..type = Tag_Type.SUPPLIER
-    //     ..status = Tag_Status.PLACED,
-    //   Tag()
-    //     ..tagId = Int64()
-    //     ..createdBy = Int64()
-    //     ..createdAt = timestamp3
-    //     ..compositeFilter = CompositeFilter()
-    //     ..geolocation = geoLocation3
-    //     ..type = Tag_Type.CLIENT
-    //     ..status = Tag_Status.BOOKMARKED,
-    //   Tag()
-    //     ..tagId = Int64()
-    //     ..createdBy = Int64()
-    //     ..createdAt = timestamp4
-    //     ..compositeFilter = CompositeFilter()
-    //     ..geolocation = geoLocation4
-    //     ..type = Tag_Type.CLIENT
-    //     ..status = Tag_Status.BOOKMARKED,
-    // ];
-    return TagResponse()..tags.addAll(await api.getTags(''));
+    final userId = retrieveUserIdFrom(request);
+    final tags = await api.getTags(userId);
+    return TagResponse(tags: tags);
   }
 
   @override
@@ -213,7 +151,8 @@ class MockTagServiceClient implements TagServiceClient {
   }
 
   Future<TagResponse> _removeTag(TagRequest request) async {
-    return TagResponse()..tags.add(await api.deleteTag('', request.tag.tagId));
+    final userId = retrieveUserIdFrom(request);
+    return TagResponse()..tags.add(await api.deleteTag(userId, request.tag.tagId));
   }
 
   @override
