7ee9eae62b208d1fd8a61361a968e2eb1b3c4199
ronxbysu <indominus@dinobitsoft.com>
2023-05-04 13:35:41 +0300

    proto

--- lib/generated/deedee/api/model/tag.pbjson.dart
+++ lib/generated/deedee/api/model/tag.pbjson.dart
@@ -15,10 +15,11 @@ const Tag$json = const {
     const {'1': 'tagId', '3': 1, '4': 1, '5': 4, '10': 'tagId'},
     const {'1': 'createdBy', '3': 2, '4': 1, '5': 4, '10': 'createdBy'},
     const {'1': 'createdAt', '3': 3, '4': 1, '5': 11, '6': '.google.protobuf.Timestamp', '10': 'createdAt'},
-    const {'1': 'compositeFilter', '3': 4, '4': 1, '5': 11, '6': '.deedee.api.filter.model.CompositeFilter', '10': 'compositeFilter'},
+    const {'1': 'compositeFilter', '3': 4, '4': 1, '5': 11, '6': '.deedee.api.compositefilter.model.CompositeFilter', '10': 'compositeFilter'},
     const {'1': 'geolocation', '3': 5, '4': 1, '5': 11, '6': '.deedee.api.geolocation.model.Geolocation', '10': 'geolocation'},
     const {'1': 'type', '3': 6, '4': 1, '5': 14, '6': '.deedee.api.tag.model.Tag.Type', '10': 'type'},
     const {'1': 'status', '3': 7, '4': 1, '5': 14, '6': '.deedee.api.tag.model.Tag.Status', '10': 'status'},
+    const {'1': 'observations', '3': 8, '4': 3, '5': 11, '6': '.deedee.api.statistic.model.Observation', '10': 'observations'},
   ],
   '4': const [Tag_Type$json, Tag_Status$json],
 };
@@ -45,4 +46,4 @@ const Tag_Status$json = const {
 };
 
 /// Descriptor for `Tag`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List tagDescriptor = $convert.base64Decode('CgNUYWcSFAoFdGFnSWQYASABKARSBXRhZ0lkEhwKCWNyZWF0ZWRCeRgCIAEoBFIJY3JlYXRlZEJ5EjgKCWNyZWF0ZWRBdBgDIAEoCzIaLmdvb2dsZS5wcm90b2J1Zi5UaW1lc3RhbXBSCWNyZWF0ZWRBdBJSCg9jb21wb3NpdGVGaWx0ZXIYBCABKAsyKC5kZWVkZWUuYXBpLmZpbHRlci5tb2RlbC5Db21wb3NpdGVGaWx0ZXJSD2NvbXBvc2l0ZUZpbHRlchJLCgtnZW9sb2NhdGlvbhgFIAEoCzIpLmRlZWRlZS5hcGkuZ2VvbG9jYXRpb24ubW9kZWwuR2VvbG9jYXRpb25SC2dlb2xvY2F0aW9uEjIKBHR5cGUYBiABKA4yHi5kZWVkZWUuYXBpLnRhZy5tb2RlbC5UYWcuVHlwZVIEdHlwZRI4CgZzdGF0dXMYByABKA4yIC5kZWVkZWUuYXBpLnRhZy5tb2RlbC5UYWcuU3RhdHVzUgZzdGF0dXMiIAoEVHlwZRIKCgZDTElFTlQQABIMCghTVVBQTElFUhABIk4KBlN0YXR1cxIOCgpCT09LTUFSS0VEEAASDgoKU1VCU0NSSUJFRBABEgoKBlBMQUNFRBACEgsKB0NIQU5HRUQQAxILCgdERUxFVEVEEAQ=');
+final $typed_data.Uint8List tagDescriptor = $convert.base64Decode('CgNUYWcSFAoFdGFnSWQYASABKARSBXRhZ0lkEhwKCWNyZWF0ZWRCeRgCIAEoBFIJY3JlYXRlZEJ5EjgKCWNyZWF0ZWRBdBgDIAEoCzIaLmdvb2dsZS5wcm90b2J1Zi5UaW1lc3RhbXBSCWNyZWF0ZWRBdBJbCg9jb21wb3NpdGVGaWx0ZXIYBCABKAsyMS5kZWVkZWUuYXBpLmNvbXBvc2l0ZWZpbHRlci5tb2RlbC5Db21wb3NpdGVGaWx0ZXJSD2NvbXBvc2l0ZUZpbHRlchJLCgtnZW9sb2NhdGlvbhgFIAEoCzIpLmRlZWRlZS5hcGkuZ2VvbG9jYXRpb24ubW9kZWwuR2VvbG9jYXRpb25SC2dlb2xvY2F0aW9uEjIKBHR5cGUYBiABKA4yHi5kZWVkZWUuYXBpLnRhZy5tb2RlbC5UYWcuVHlwZVIEdHlwZRI4CgZzdGF0dXMYByABKA4yIC5kZWVkZWUuYXBpLnRhZy5tb2RlbC5UYWcuU3RhdHVzUgZzdGF0dXMSSwoMb2JzZXJ2YXRpb25zGAggAygLMicuZGVlZGVlLmFwaS5zdGF0aXN0aWMubW9kZWwuT2JzZXJ2YXRpb25SDG9ic2VydmF0aW9ucyIgCgRUeXBlEgoKBkNMSUVOVBAAEgwKCFNVUFBMSUVSEAEiTgoGU3RhdHVzEg4KCkJPT0tNQVJLRUQQABIOCgpTVUJTQ1JJQkVEEAESCgoGUExBQ0VEEAISCwoHQ0hBTkdFRBADEgsKB0RFTEVURUQQBA==');
