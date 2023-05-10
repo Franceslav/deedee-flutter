f5c7ae1e3729fc3d26bec1ecf9e1f3b04dd851cd
TemityStolyarov <127617131+TemityStolyarov@users.noreply.github.com>
2023-05-10 17:38:46 +0300

    minor fixes for pr #336
    fixed issues in pull request #336 for issue #297 that mentioned in the comments

--- lib/ui/page/add_card/add_card_screen.dart
+++ lib/ui/page/add_card/add_card_screen.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:deedee/generated/deedee/api/service/payment_service.pb.dart';
+import 'package:deedee/injection.dart';
+import 'package:deedee/repository/card_repository.dart';
 import 'package:deedee/repository/payment_method_repository.dart';
 import 'package:deedee/ui/deedee_button/deedee_button.dart';
 import 'package:deedee/ui/global_widgets/deedee_appbar.dart';
@@ -32,128 +34,140 @@ class _CardScreenState extends State<CardScreen> {
   @override
   Widget build(BuildContext context) {
     final locale = AppLocalizations.of(context)!;
-    return BlocConsumer<CardBloc, CardState>(
-      listener: (context, state) {},
-      builder: (context, state) {
-        if (state is UpdateCardState) {
-          final cardModel = state.cardModel;
-          String cardNumber = cardModel.cardNumber;
-          String expiryDate = cardModel.expiryDate;
-          String cardHolderName = cardModel.cardHolderName;
-          String cvvCode = cardModel.cvvCode;
-          return Scaffold(
-            appBar: DeeDeeAppBar(
-              title: locale.addCard,
-              controller: _controller,
-              child: const ProfilePhotoWithBadge(),
-            ),
-            body: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 16, bottom: 16),
-                child: Column(
-                  children: [
-                    CardWidget(
-                      isCvvFocused: isCvvFocused,
-                      cardNumber: cardNumber,
-                      expiryDate: expiryDate,
-                      cardHolderName: cardHolderName,
-                      cvvCode: cvvCode,
-                      color: Colors.deepPurple,
-                    ),
-                    Expanded(
-                        child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          CreditCardForm(
-                              formKey: formKey,
-                              obscureCvv: true,
-                              obscureNumber: false,
-                              cardNumber: cardNumber,
-                              cvvCode: cvvCode,
-                              isHolderNameVisible: true,
-                              isCardNumberVisible: true,
-                              isExpiryDateVisible: true,
-                              cardHolderName: cardHolderName,
-                              expiryDate: expiryDate,
-                              themeColor: Colors.purple,
-                              textColor: Colors.black,
-                              cardNumberDecoration: InputDecoration(
-                                labelText: locale.cardNumber,
-                                hintText: 'XXXX XXXX XXXX XXXX',
-                                hintStyle: const TextStyle(color: Colors.grey),
-                                labelStyle: const TextStyle(color: Colors.grey),
-                                focusedBorder: border,
-                                enabledBorder: border,
-                              ),
-                              expiryDateDecoration: InputDecoration(
-                                hintStyle: const TextStyle(color: Colors.grey),
-                                labelStyle: const TextStyle(color: Colors.grey),
-                                focusedBorder: border,
-                                enabledBorder: border,
-                                labelText: locale.expiredDate,
-                                hintText: 'XX/XX',
-                              ),
-                              cvvCodeDecoration: InputDecoration(
-                                hintStyle: const TextStyle(color: Colors.grey),
-                                labelStyle: const TextStyle(color: Colors.grey),
-                                focusedBorder: border,
-                                enabledBorder: border,
-                                labelText: 'CVV',
-                                hintText: 'XXX',
-                              ),
-                              cardHolderDecoration: InputDecoration(
-                                hintStyle: const TextStyle(color: Colors.grey),
-                                labelStyle: const TextStyle(color: Colors.grey),
-                                focusedBorder: border,
-                                enabledBorder: border,
-                                labelText: locale.cardHolder,
-                              ),
-                              onCreditCardModelChange:
-                                  (CreditCardModel? creditCardModel) {
-                                context.read<CardBloc>().add(
-                                    CardModelChangedEvent(
-                                        cardModel: creditCardModel));
-                              }),
-                        ],
+    final bloc = CardBloc(
+      locator.get<CardRepository>(),
+    );
+    return BlocProvider<CardBloc>(
+      create: (_) => bloc,
+      child: BlocConsumer<CardBloc, CardState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          if (state is UpdateCardState) {
+            final cardModel = state.cardModel;
+            String cardNumber = cardModel.cardNumber;
+            String expiryDate = cardModel.expiryDate;
+            String cardHolderName = cardModel.cardHolderName;
+            String cvvCode = cardModel.cvvCode;
+            return Scaffold(
+              appBar: DeeDeeAppBar(
+                title: locale.addCard,
+                controller: _controller,
+                child: const ProfilePhotoWithBadge(),
+              ),
+              body: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 16),
+                  child: Column(
+                    children: [
+                      CardWidget(
+                        isCvvFocused: isCvvFocused,
+                        cardNumber: cardNumber,
+                        expiryDate: expiryDate,
+                        cardHolderName: cardHolderName,
+                        cvvCode: cvvCode,
+                        color: Colors.deepPurple,
                       ),
-                    )),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Column(
-                        children: [
-                          DeeDeeButton(
-                              title: locale.addCard,
-                              onPressed: validateForm()
-                                  ? () {
-                                      context.read<CardBloc>().add(
+                      Expanded(
+                          child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            CreditCardForm(
+                                formKey: formKey,
+                                obscureCvv: true,
+                                obscureNumber: false,
+                                cardNumber: cardNumber,
+                                cvvCode: cvvCode,
+                                isHolderNameVisible: true,
+                                isCardNumberVisible: true,
+                                isExpiryDateVisible: true,
+                                cardHolderName: cardHolderName,
+                                expiryDate: expiryDate,
+                                themeColor: Colors.purple,
+                                textColor: Colors.black,
+                                cardNumberDecoration: InputDecoration(
+                                  labelText: locale.cardNumber,
+                                  hintText: 'XXXX XXXX XXXX XXXX',
+                                  hintStyle:
+                                      const TextStyle(color: Colors.grey),
+                                  labelStyle:
+                                      const TextStyle(color: Colors.grey),
+                                  focusedBorder: border,
+                                  enabledBorder: border,
+                                ),
+                                expiryDateDecoration: InputDecoration(
+                                  hintStyle:
+                                      const TextStyle(color: Colors.grey),
+                                  labelStyle:
+                                      const TextStyle(color: Colors.grey),
+                                  focusedBorder: border,
+                                  enabledBorder: border,
+                                  labelText: locale.expiredDate,
+                                  hintText: 'XX/XX',
+                                ),
+                                cvvCodeDecoration: InputDecoration(
+                                  hintStyle:
+                                      const TextStyle(color: Colors.grey),
+                                  labelStyle:
+                                      const TextStyle(color: Colors.grey),
+                                  focusedBorder: border,
+                                  enabledBorder: border,
+                                  labelText: 'CVV',
+                                  hintText: 'XXX',
+                                ),
+                                cardHolderDecoration: InputDecoration(
+                                  hintStyle:
+                                      const TextStyle(color: Colors.grey),
+                                  labelStyle:
+                                      const TextStyle(color: Colors.grey),
+                                  focusedBorder: border,
+                                  enabledBorder: border,
+                                  labelText: locale.cardHolder,
+                                ),
+                                onCreditCardModelChange:
+                                    (CreditCardModel? creditCardModel) {
+                                  context.read<CardBloc>().add(
+                                      CardModelChangedEvent(
+                                          cardModel: creditCardModel));
+                                }),
+                          ],
+                        ),
+                      )),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Column(
+                          children: [
+                            DeeDeeButton(
+                                title: locale.addCard,
+                                onPressed: validateForm()
+                                    ? () {
+                                        bloc.add(
                                           SaveCardDataEvent(
-                                              cardModel: cardModel));
-                                      PaymentMethodRequestRepository
-                                          paymentMethodRequestRepository =
-                                          PaymentMethodRequestRepository();
-                                      paymentMethodRequestRepository
-                                          .addPaymentMethod(
-                                        PaymentMethodRequest(),
-                                        cardModel,
-                                      );
-                                      Navigator.pop(context);
-                                    }
-                                  : null,
-                              gradientButton: true),
-                        ],
-                      ),
-                    )
-                  ],
+                                              cardModel: cardModel),
+                                        );
+                                        PaymentMethodRequestRepository()
+                                            .addPaymentMethod(
+                                          PaymentMethodRequest(),
+                                          cardModel,
+                                        );
+                                        Navigator.pop(context);
+                                      }
+                                    : null,
+                                gradientButton: true),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        }
-        return const CircularProgressIndicator();
-      },
+            );
+          }
+          return const CircularProgressIndicator();
+        },
+      ),
     );
   }
 
