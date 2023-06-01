735372975464a5ff028e6fc17fcc4b858ae5c182
Bohdan Kim <user576g@gmail.com>
2023-06-01 16:19:40 +0300

    Upload Photo Service IP and port is saved in Shared Preferences and used in HttpService.

--- lib/ui/page/settings/upload_settings/upload_settings_screen.dart
+++ lib/ui/page/settings/upload_settings/upload_settings_screen.dart
@@ -2,7 +2,6 @@ import 'package:deedee/constants.dart';
 import 'package:deedee/injection.dart';
 import 'package:deedee/services/helper.dart';
 import 'package:deedee/services/shared.dart';
-import 'package:deedee/ui/page/home/home_screen.dart';
 import 'package:deedee/ui/page/settings/upload_settings/upload_settings_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,7 +10,7 @@ class UploadSettingsScreen extends StatefulWidget {
   const UploadSettingsScreen({super.key});
 
   @override
-  _UploadSettingsScreenState createState() =>
+  State<UploadSettingsScreen> createState() =>
       _UploadSettingsScreenState();
 }
 
@@ -22,13 +21,15 @@ class _UploadSettingsScreenState extends State<UploadSettingsScreen> {
 
   AutovalidateMode _validate = AutovalidateMode.disabled;
 
-  TextEditingController ipAddressController =
-      TextEditingController(text: "127.0.0.1");
-  TextEditingController portController = TextEditingController(text: "1234");
+  final TextEditingController _ipAddressController = TextEditingController();
+  final TextEditingController _portController = TextEditingController();
+  final _sharedUtils = locator.get<SharedUtils>();
 
   @override
   void initState() {
     super.initState();
+    _ipAddressController.text = _sharedUtils.getUploadPhotoServiceIpAddress() ?? "null";
+    _portController.text = _sharedUtils.getUploadPhotoServicePort() ?? "null";
   }
 
   @override
@@ -37,9 +38,7 @@ class _UploadSettingsScreenState extends State<UploadSettingsScreen> {
         create: (context) => UploadSettingsCubit(),
         child: Builder(builder: (context) {
           return WillPopScope(
-              onWillPop: () async {
-                return pushReplacement(context, const HomeScreen());
-              },
+              onWillPop: () async => true,
               child: Scaffold(
                 appBar: AppBar(
                   backgroundColor: Colors.transparent,
@@ -52,8 +51,8 @@ class _UploadSettingsScreenState extends State<UploadSettingsScreen> {
                   listenWhen: (old, current) => old != current,
                   listener: (context, state) {
                     if (state is ValidUploadSettingsField) {
-                      locator.get<SharedUtils>().saveIpAddress(state.host);
-                      locator.get<SharedUtils>().savePort(state.port);
+                      _sharedUtils.saveUploadPhotoServiceIpAddress(state.host);
+                      _sharedUtils.saveUploadPhotoServicePort(state.port);
                     } else if (state is UploadSettingsFailureState) {
                       showSnackBar(context, state.errorMessage);
                     }
@@ -76,105 +75,83 @@ class _UploadSettingsScreenState extends State<UploadSettingsScreen> {
                               padding: EdgeInsets.only(
                                   top: 32.0, right: 16.0, left: 16.0),
                               child: Text(
-                                'IP Address',
+                                'Host/IP Address of Photo Upload Service',
                                 style: TextStyle(
                                     color: Color(COLOR_PRIMARY),
                                     fontSize: 25.0,
                                     fontWeight: FontWeight.bold),
                               ),
                             ),
-                            FutureBuilder(
-                              future: locator
-                                  .get<SharedUtils>()
-                                  .getPrefsIpAddress(),
-                              builder:
-                                  (context, AsyncSnapshot<String?> snapshot) {
-                                if (snapshot.hasData) {
-                                  // _ipAddress = ;
-                                  ipAddressController.text = snapshot.data!;
-                                }
-                                return Column(
-                                  crossAxisAlignment:
-                                      CrossAxisAlignment.stretch,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          top: 32.0, right: 24.0, left: 24.0),
-                                      child: TextFormField(
-                                        controller: ipAddressController,
-                                        textAlignVertical:
-                                            TextAlignVertical.center,
-                                        textInputAction: TextInputAction.done,
-                                        validator: validateTopic,
-                                        onFieldSubmitted: (_) => context
-                                            .read<UploadSettingsCubit>()
-                                            .checkValidField(
-                                                _key, _host, _port),
-                                        onSaved: (val) => _host = val!,
-                                        style: const TextStyle(fontSize: 18.0),
-                                        keyboardType: TextInputType.name,
-                                        cursorColor: const Color(COLOR_PRIMARY),
-                                        decoration: getInputDecoration(
-                                            hint: 'IP Address',
-                                            darkMode: isDarkMode(context),
-                                            errorColor:
-                                                Theme.of(context).errorColor),
-                                      ),
-                                    ),
-                                  ],
-                                );
-                              },
+                            Column(
+                              crossAxisAlignment:
+                              CrossAxisAlignment.stretch,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 5.0, right: 24.0, left: 24.0),
+                                  child: TextFormField(
+                                    controller: _ipAddressController,
+                                    textAlignVertical:
+                                    TextAlignVertical.center,
+                                    textInputAction: TextInputAction.done,
+                                    validator: validateTopic,
+                                    onFieldSubmitted: (_) => context
+                                        .read<UploadSettingsCubit>()
+                                        .checkValidField(
+                                        _key, _host, _port),
+                                    onSaved: (val) => _host = val!,
+                                    style: const TextStyle(fontSize: 18.0),
+                                    keyboardType: TextInputType.name,
+                                    cursorColor: const Color(COLOR_PRIMARY),
+                                    decoration: getInputDecoration(
+                                        hint: 'IP Address',
+                                        darkMode: isDarkMode(context),
+                                        errorColor:
+                                        Theme.of(context).errorColor),
+                                  ),
+                                ),
+                              ],
                             ),
                             const Padding(
                               padding: EdgeInsets.only(
                                   top: 32.0, right: 16.0, left: 16.0),
                               child: Text(
-                                'Port',
+                                'Port of Photo Upload Service',
                                 style: TextStyle(
                                     color: Color(COLOR_PRIMARY),
                                     fontSize: 25.0,
                                     fontWeight: FontWeight.bold),
                               ),
                             ),
-                            FutureBuilder(
-                              future: locator.get<SharedUtils>().getPrefsPort(),
-                              builder:
-                                  (context, AsyncSnapshot<String?> snapshot) {
-                                if (snapshot.hasData) {
-                                  // _port = ;
-                                  portController.text = snapshot.data!;
-                                }
-                                return Column(
-                                  crossAxisAlignment:
-                                      CrossAxisAlignment.stretch,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          top: 32.0, right: 24.0, left: 24.0),
-                                      child: TextFormField(
-                                        controller: portController,
-                                        textAlignVertical:
-                                            TextAlignVertical.center,
-                                        textInputAction: TextInputAction.done,
-                                        validator: validateTopic,
-                                        onFieldSubmitted: (_) => context
-                                            .read<UploadSettingsCubit>()
-                                            .checkValidField(
-                                                _key, _host, _port),
-                                        onSaved: (val) => _host = val!,
-                                        style: const TextStyle(fontSize: 18.0),
-                                        keyboardType: TextInputType.name,
-                                        cursorColor: const Color(COLOR_PRIMARY),
-                                        decoration: getInputDecoration(
-                                            hint: 'Port',
-                                            darkMode: isDarkMode(context),
-                                            errorColor:
-                                                Theme.of(context).errorColor),
-                                      ),
-                                    ),
-                                  ],
-                                );
-                              },
+                            Column(
+                              crossAxisAlignment:
+                              CrossAxisAlignment.stretch,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 5.0, right: 24.0, left: 24.0),
+                                  child: TextFormField(
+                                    controller: _portController,
+                                    textAlignVertical:
+                                    TextAlignVertical.center,
+                                    textInputAction: TextInputAction.done,
+                                    validator: validateTopic,
+                                    onFieldSubmitted: (_) => context
+                                        .read<UploadSettingsCubit>()
+                                        .checkValidField(
+                                        _key, _host, _port),
+                                    onSaved: (val) => _port = val!,
+                                    style: const TextStyle(fontSize: 18.0),
+                                    keyboardType: TextInputType.name,
+                                    cursorColor: const Color(COLOR_PRIMARY),
+                                    decoration: getInputDecoration(
+                                        hint: 'Port',
+                                        darkMode: isDarkMode(context),
+                                        errorColor:
+                                        Theme.of(context).errorColor),
+                                  ),
+                                ),
+                              ],
                             ),
                             Padding(
                               padding: const EdgeInsets.only(
@@ -204,8 +181,8 @@ class _UploadSettingsScreenState extends State<UploadSettingsScreen> {
                                         .read<UploadSettingsCubit>()
                                         .checkValidField(
                                             _key,
-                                            ipAddressController.text,
-                                            portController.text);
+                                            _ipAddressController.text,
+                                            _portController.text);
                                   }),
                             ),
                           ],
