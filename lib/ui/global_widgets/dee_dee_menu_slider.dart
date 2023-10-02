64cacfe39b70ec3c2bd9eeefd26702d7e2f5eabc
Igor <57453357+misterio-smit@users.noreply.github.com>
2023-10-02 13:40:31 +0700

    Deemob 297 move main page menu into profile menu (#499)
    * move main page menu into profile menu
    
    ---------
    
    Co-authored-by: Igor Gutsu <misterriobit@gmail.com>

--- lib/ui/global_widgets/dee_dee_menu_slider.dart
+++ lib/ui/global_widgets/dee_dee_menu_slider.dart
@@ -9,22 +9,59 @@ import '../routes/app_router.gr.dart';
 import 'genaral_sliding_panel/general_sliding_panel.dart';
 import 'genaral_sliding_panel/general_sliding_panel_item.dart';
 
+// DEPRECATED
 class DeeDeeMenuSlider extends GeneralSlidingPanel {
   final User user;
   final BuildContext context;
 
   DeeDeeMenuSlider(
-      this.context, {
-        super.key,
-        required super.controller,
-        required this.user,
-      }) : super(
-      maxHeight: MediaQuery.of(context).size.height * 0.4,
-      generalSlidingPanelItemList: [
-        GeneralSlidingPanelItem(
-          icon: Icons.qr_code_scanner,
-          text: AppLocalizations.of(context)!.qrScanner,
-          onTap: () async {},
-        ),
-      ]);
+    this.context, {
+    super.key,
+    required super.controller,
+    required this.user,
+  }) : super(
+            maxHeight: MediaQuery.of(context).size.height * 0.4,
+            generalSlidingPanelItemList: [
+              GeneralSlidingPanelItem(
+                icon: Icons.qr_code_scanner,
+                text: AppLocalizations.of(context)!.qrScanner,
+                onTap: () async {
+                  context.router.navigate(const BookmarkQRScannerRoute());
+                },
+              ),
+              GeneralSlidingPanelItem(
+                  icon: Icons.account_box,
+                  text: AppLocalizations.of(context)!.profile,
+                  onTap: () {
+                    if (context.router.current.isActive) {
+                      controller.close();
+                    }
+                    context.router.navigate(const AccountScreenRoute());
+                  }),
+              GeneralSlidingPanelItem(
+                  icon: Icons.settings,
+                  text: AppLocalizations.of(context)!.settings,
+                  onTap: () {
+                    if (context.router.current.isActive) {
+                      controller.close();
+                    }
+                    context.router.navigate(const SettingsScreenRoute());
+                  }),
+              GeneralSlidingPanelItem(
+                  icon: Icons.help_outline,
+                  text: AppLocalizations.of(context)!.helpTitle,
+                  onTap: () {
+                    if (context.router.current.isActive) {
+                      controller.close();
+                    }
+                    context.router.navigate(const HelpScreenRoute());
+                  }),
+              GeneralSlidingPanelItem(
+                  icon: Icons.exit_to_app,
+                  text: AppLocalizations.of(context)!.logout,
+                  onTap: () {
+                    context.read<AuthenticationBloc>().add(LogoutEvent());
+                    context.router.replace(const LoginScreenRoute());
+                  }),
+            ]);
 }
