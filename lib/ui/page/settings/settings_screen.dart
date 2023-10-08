c21b4d7641f015b581d2d8b138576bbf047023eb
Glushenkov <romaglushenkov02@gmail.com>
2023-10-08 16:53:03 +0400

    [DEEMOB-279]: implement settings page design

--- lib/ui/page/settings/settings_screen.dart
+++ lib/ui/page/settings/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:deedee/ui/auth/authentication_bloc.dart';
 import 'package:deedee/ui/auth/biometric/bloc/biometric_bloc.dart';
 import 'package:deedee/ui/global_widgets/dee_dee_menu_slider.dart';
 import 'package:deedee/ui/global_widgets/deedee_appbar.dart';
@@ -6,6 +7,7 @@ import 'package:deedee/ui/global_widgets/profile_photo_with_badge.dart';
 import 'package:deedee/ui/page/account/account_language_dropdown.dart';
 import 'package:deedee/ui/page/settings/settings_cubit.dart';
 import 'package:deedee/ui/routes/app_router.gr.dart';
+import 'package:deedee/ui/theme/colors.dart';
 import 'package:deedee/ui/user_bloc/user_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -32,102 +34,67 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     final user = context.select((UserBloc bloc) => bloc.state.user);
-    return BlocProvider<SettingsCubit>(
-        create: (context) => SettingsCubit(),
-        child: Builder(builder: (context) {
-          return Scaffold(
-              appBar: DeeDeeAppBar(
-                title: AppLocalizations.of(context)!.settings,
-                controller: _controller,
-                child: const ProfilePhotoWithBadge(),
-              ),
-              body: BlocProvider(
-                  create: (context) => BiometricBloc(),
-                  child: BlocConsumer<BiometricBloc, BiometricState>(
-                    listener: (context, state) {
-                      if (state is BiometricOn) {
-                        biometric = !biometric;
-                      }
-                    },
-                    builder: (context, state) {
-                      return Stack(
-                        children: [
-                          SettingsList(
-                            sections: [
-                              SettingsSection(
-                                title: Text(
-                                    AppLocalizations.of(context)!.qrScanner),
-                                tiles: [
-                                  SettingsTile(
-                                    title: Text(AppLocalizations.of(context)!
-                                        .qrScanner),
-                                    leading: const Icon(Icons.qr_code_scanner),
-                                    onPressed: (BuildContext context) {
-                                      context.router
-                                          .push(const BookmarkQRScannerRoute());
-                                    },
-                                  ),
-                                ],
-                              ),
-                              SettingsSection(
-                                title: Text(AppLocalizations.of(context)!
-                                    .connectionSettings),
-                                tiles: [
-                                  SettingsTile(
-                                    title: Text(AppLocalizations.of(context)!
-                                        .settingIpAddressPort),
-                                    leading:
-                                        const Icon(Icons.settings_ethernet),
-                                    onPressed: (BuildContext context) {
-                                      context.router.push(
-                                          const ConnectionSettingsScreenRoute());
-                                    },
-                                  ),
-                                  SettingsTile(
-                                    title: const Text('Photo upload host'),
-                                    leading: const Icon(Icons.upload_sharp),
-                                    onPressed: (BuildContext context) {
-                                      context.router.push(
-                                          const UploadSettingsScreenRoute());
-                                    },
-                                  ),
-                                ],
-                              ),
-                              SettingsSection(
-                                title: const Text('Security'),
-                                tiles: [
-                                  SettingsTile.switchTile(
-                                    title: Text(AppLocalizations.of(context)!
-                                        .useFingerprint),
-                                    leading: const Icon(Icons.fingerprint),
-                                    onToggle: (biometric) {
-                                      context.read<BiometricBloc>().add(
-                                          ToggleBiometric(
-                                              biometric: biometric));
-                                    },
-                                    initialValue: biometric,
-                                  ),
-                                ],
-                              ),
-                              SettingsSection(
-                                title: const Text('Language'),
-                                tiles: [
-                                  SettingsTile(
-                                    title: LanguagesExpansionTile(),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                          ProfileMenuSlider(
-                            context,
-                            controller: _controller,
-                            user: user,
-                          ),
-                        ],
-                      );
-                    },
-                  )));
-        }));
+    return Scaffold(
+        appBar: DeeDeeAppBar(
+          title: AppLocalizations.of(context)!.settings,
+          controller: _controller,
+          child: const Icon(Icons.menu),
+        ),
+        body: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(children: [
+                const SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: AppColors.lightgrey,
+                    prefixIcon: Image.asset('assets/images/search_icon.png'),
+                    hintText: AppLocalizations.of(context)!.search,
+                    hintStyle: const TextStyle(color: AppColors.grey),
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 10,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 15),
+                ListTile(
+                  title: Text(AppLocalizations.of(context)!.notification),
+                  trailing: Image.asset('assets/images/chevron_right_icon.png'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text(AppLocalizations.of(context)!.addAccount),
+                  trailing: Image.asset('assets/images/chevron_right_icon.png'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: Text(AppLocalizations.of(context)!.logout,
+                      style: TextStyle(color: AppColors.red)),
+                  onTap: () {
+                    context.read<AuthenticationBloc>().add(LogoutEvent());
+                    context.router.replace(const LoginScreenRoute());
+                  },
+                ),
+                ListTile(
+                  title: Text(AppLocalizations.of(context)!.deleteAccount,
+                      style: TextStyle(color: AppColors.grey)),
+                  onTap: () {},
+                ),
+              ]),
+            ),
+            ProfileMenuSlider(
+              context,
+              controller: _controller,
+              user: user,
+            ),
+          ],
+        ));
   }
 }
